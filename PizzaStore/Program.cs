41c77770d3db1766fc76d8e996ad93c8e61efd0a
lidiaAsamnew <lidia.yirbe@gmail.com>
2025-11-28 19:18:07 +0300

    Implement EF Core Model (PizzaStore) and SQLite Configuration

---
+++ PizzaStore/Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PizzaStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Pizzas")
+                       ?? "Data Source=Pizzas.db";
+
+builder.Services.AddSqlite<PizzaDb>(connectionString);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PizzaStore API",
+        Description = "Pizza API using SQLite",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello SQLite!");
+
+// CRUD Endpoints
+
+app.MapGet("/pizzas", async (PizzaDb db) =>
+    await db.Pizzas.ToListAsync()
+);
+
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) =>
+    await db.Pizzas.FindAsync(id)
+);
+
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatePizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+
+    pizza.Name = updatePizza.Name;
+    pizza.Description = updatePizza.Description;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.Run();
